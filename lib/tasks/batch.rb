9295d339ab1b2ed277cf78b36b5b722ee4506e11
TakuKobayshi <>
2017-06-19 12:17:36 +0900

    ちょっとBatch処理修正

--- lib/tasks/batch.rb
+++ lib/tasks/batch.rb
@@ -4,8 +4,9 @@ namespace :batch do
   end
 
   task bot_tweet: :environment do
-    Event.where("started_at > ?", Time.current).find_each do |event|
-      if event.hackathon_event? && !TwitterBot.exists?(from: event)
+    future_events = Event.where("? < started_at AND started_at < ?", Time.current, 1.year.since).order("started_at ASC")
+    future_events.each do |event|
+      if !TwitterBot.exists?(from: event)
         TwitterBot.tweet!(text: event.generate_tweet_text, from: event, options: {lat: event.lat, long: event.lon})
       end
     end
