92908d204e6a1bbe5a2ccb5f9faa7e8067c2dc32
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-06-20 22:08:18 +0900

    ue4yochiでハッシュタグついたら自動的にリツイート&いいね

--- scripts/twitter_bot_stream_tweet.rb
+++ scripts/twitter_bot_stream_tweet.rb
@@ -2,5 +2,10 @@ twitter_rest_client = TwitterBot.get_twitter_rest_client
 twitter_stream_client = TwitterBot.get_twitter_stream_client
 
 twitter_stream_client.track('#ue4yochi', 'blender', 'ue4', 'unreal', 'engine') do |status|
-  p status.to_json
+#twitter_stream_client.sample do |status|
+  next if status.retweeted? || status.user.screen_name == "ue4BlenderYochi"
+  if status.hashtags.any?{|h| h.text == "ue4yochi" }
+    twitter_rest_client.retweet(status.id)
+    twitter_rest_client.favorite(status.id)
+  end
 end
